ada4a77493052e5feaa73586376fd3f46bd8259b
Alejandro Suarez <alex-suarez@live.com>
2023-08-27 00:12:25 -0600

    create the main files

---
+++ batch.py
@@ -0,0 +1,8 @@
+
+BATCH_CAPACITY = 5
+
+class Batch:
+    
+    def __init__(self, name: str, ): 
+        pass
+        
---
+++ ui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import time
+
+# create a window to insert data
+window = Tk()
+window.title("Batch Processing")
+window.geometry("400x300")
+window.resizable(width=False, height=False)
+
+
+
+
+
+window.mainloop()
